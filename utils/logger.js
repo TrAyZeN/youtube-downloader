6ef4683cce7674700bdf42fcde5b65a8a2db3406
TrAyZeN <1810leo@gmail.com>
2020-02-15 20:25:30 +0100

    Add logger

---
+++ utils/logger.js
@@ -0,0 +1,60 @@
+const consoleColor = {
+    reset: "\x1b[0m",
+
+    black: "\x1b[30m",
+    red: "\x1b[31m",
+    green: "\x1b[32m",
+    yellow: "\x1b[33m",
+    blue: "\x1b[34m",
+    magenta: "\x1b[35m",
+    cyan: "\x1b[36m",
+    white: "\x1b[37m",
+
+    blackBg: "\x1b[40m",
+    redBg: "\x1b[41m",
+    greenBg: "\x1b[42m",
+    yellowBg: "\x1b[43m",
+    blueBg: "\x1b[44m",
+    magentaBg: "\x1b[45m",
+    cyanBg: "\x1b[46m",
+    whiteBg: "\x1b[47m"
+};
+
+function colorString(str, color) {
+    return color + str + consoleColor.reset;
+}
+
+function debug(message) {
+    console.log(`${formatedCurrentDate()} ${colorString("[DEBUG]", consoleColor.green)} ${message}`);
+}
+
+function info(message) {
+    console.log(`${formatedCurrentDate()} ${colorString("[INFO ]", consoleColor.yellow)} ${message}`);
+}
+
+function error(message) {
+    console.error(`${formatedCurrentDate()} ${colorString("[ERROR]", consoleColor.red)} ${message}`);
+}
+
+function formatedCurrentDate() {
+    const now = new Date();
+    const date = `${zfill(now.getDate(), 2)}-${zfill(now.getMonth() + 1, 2)}-${zfill(now.getFullYear(), 4)}`;
+    const time = `${zfill(now.getHours(), 2)}:${zfill(now.getMinutes(), 2)}:${zfill(now.getSeconds(), 2)}`;
+
+    return `${date} ${time}`;
+}
+
+function zfill(str, n) {
+    str = str.toString();
+    n = n - str.length;
+
+    for (let i = 0; i < n; i++)
+        str = "0" + str;
+
+    return str;
+}
+
+module.exports = {
+    info,
+    error
+};
