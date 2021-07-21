aeafad85ae1b481bea758e9d0470c97418b49842
TrAyZeN <1810leo@gmail.com>
2021-07-21 17:58:17 +0200

    Add input to download video and progress bar

--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -1,30 +1,76 @@
 <script>
-	export let name;
+    import axios from 'axios';
+    import ProgressBar from './ProgressBar.svelte';
+
+    const focus = node => node.focus();
+    let url = '';
+    let eventSource = null;
+    let progress = 0;
+
+    function yeas() {
+        if (eventSource != null) {
+            return;
+        }
+
+        eventSource = new EventSource(
+            `http://localhost:3001/api/convert?url=${encodeURIComponent(url)}&format=mp3`
+        );
+
+        eventSource.addEventListener('message', (e) => {
+            console.log(`Got a message: ${e.data}`);
+            progress = 0;
+        });
+
+        eventSource.addEventListener('progress', (e) => {
+            console.log(`Got progress: ${e.data}`);
+            progress = Math.floor(JSON.parse(e.data).progress * 100);
+        });
+
+        eventSource.addEventListener('done', (e) => {
+            progress = 100;
+            eventSource.close();
+            eventSource = null;
+        });
+
+        eventSource.addEventListener('error', (e) => {
+            console.log(e);
+            eventSource.close();
+            eventSource = null;
+        });
+    }
 </script>
 
 <main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+    <h1>Youtube Downloader</h1>
+    <input
+        bind:value={url}
+        use:focus
+        type="text"
+        placeholder="Enter the url of the video you want to download"
+    >
+    <button on:click={yeas}>Download</button>
+    <ProgressBar progress={progress} />
 </main>
 
 <style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
+    main {
+        text-align: center;
+        padding: 1em;
+        max-width: 240px;
+        margin: 0 auto;
+        background-color: #141414;
+    }
 
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
+    h1 {
+        color: #ff3e00;
+        text-transform: uppercase;
+        font-size: 4em;
+        font-weight: 100;
+    }
 
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
+    @media (min-width: 640px) {
+        main {
+            max-width: none;
+        }
+    }
 </style>
---
+++ frontend/src/ProgressBar.svelte
@@ -0,0 +1,32 @@
+<script>
+    // Progress of the bar in percent
+    export let progress;
+    // Color of the bar
+    export let barColor = "green";
+    // Color of the container
+    export let containerColor = "red";
+    // Border radius in pixel
+    export let radius = 6;
+</script>
+
+<main style="
+    background-color: {containerColor};
+    border-radius: {radius}px
+">
+    <div class="bar" style="
+        width: {progress}%;
+        background-color: {barColor};
+        border-radius: {radius}px;
+    ">
+    </div>
+</main>
+
+<style>
+    main {
+        height: 20px;
+    }
+
+    .bar {
+        height: 100%;
+    }
+</style>
